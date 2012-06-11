fc6cb1b0c3bb7a2a535cd69b74e3b02a4e8e125a
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2012-06-11 01:27:15 +0100

    Initial Commit

---
+++ iJVM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iJVM
+//
+//  Created by Joao Pedro Carvalho on 6/11/12.
+//  Copyright (c) 2012 IST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iJVM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iJVM
+//
+//  Created by Joao Pedro Carvalho on 6/11/12.
+//  Copyright (c) 2012 IST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
