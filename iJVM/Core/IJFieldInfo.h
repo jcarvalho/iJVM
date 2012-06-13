c222d7010fe83e5950a98c1d7871c117824853b0
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2012-06-13 01:09:25 +0100

    Added missing FieldInfo class

---
+++ iJVM/Core/IJFieldInfo.h
@@ -0,0 +1,13 @@
+//
+//  IJFieldInfo.h
+//  iJVM
+//
+//  Created by Joao Pedro Carvalho on 6/13/12.
+//  Copyright (c) 2012 IST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IJFieldInfo : NSObject
+
+@end
---
+++ iJVM/Core/IJFieldInfo.m
@@ -0,0 +1,13 @@
+//
+//  IJFieldInfo.m
+//  iJVM
+//
+//  Created by Joao Pedro Carvalho on 6/13/12.
+//  Copyright (c) 2012 IST. All rights reserved.
+//
+
+#import "IJFieldInfo.h"
+
+@implementation IJFieldInfo
+
+@end
